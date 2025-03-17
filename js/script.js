404fcfada8d49c7e50830a68672345195e67bf15
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2025-03-17 15:22:52 -0400

    Corrected placeholder code

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const guessedLettersList = document.querySelector(
 const button = document.querySelector('.guess');
 //Selector for the input field where the player will guess a letter
 const guessInput =
-	document.querySelector('input');
+	document.querySelector('.letter');
 //Selector for the paragraph where the word will appear
 const wordInProgress = document.querySelector(
 	'.word-in-progress'
@@ -24,21 +24,38 @@ const guessResponse =
 const playAgain = document.querySelector(
 	'.play-again'
 );
+
 //Starting word for game
 const word = 'magnolia';
 
 //Determines number of ● displayed instead of letters at start
-const wordLength = function () {
-	const number = word.length;
-	wordInProgress.innerText = '●'.repeat(number);
-	console.log(wordInProgress);
+
+//My Original Response
+// const wordLength = function () {
+// 	const number = word.length;
+// 	wordInProgress.innerText = '●'.repeat(number);
+// 	console.log(wordInProgress);
+// };
+// wordLength(word);
+
+//Skilcrush solution
+// Display our symbol as placeholders for the chosen word's letters
+const placeholder = function (word) {
+	const placeholderLetters = [];
+	for (const letter of word) {
+		console.log(letter);
+		placeholderLetters.push('●');
+	}
+	wordInProgress.innerText =
+		placeholderLetters.join('');
 };
 
-wordLength(word);
+placeholder(word);
 
+//Activates operations connected to the "Guess!" button
 button.addEventListener('click', function (e) {
-	e.preventDefault();
-	let input = guessInput.value;
+	e.preventDefault(); //prevents page from reloading everytime button is pressed
+	let input = guessInput.value; //Detects the value entered into the input box
 	console.log(input);
-	guessInput.value = '';
+	guessInput.value = ''; //Clears the input box
 });
